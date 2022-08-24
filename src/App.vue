b59823f03e61c7a08a8556e8b7bfd77dbe6d115f
SI_Xiaolong <sixiaolong2018@outlook.com>
2022-08-24 15:46:36 +0800

    精简代码和Electron-Vue模块解耦。

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,8 @@ import { marked } from 'marked'
 import { debounce } from 'lodash-es'
 
 const fs = require("fs")
+// 渲染进程接收主进程消息
+const { ipcRenderer } = require("electron");
 
 export default {
   data() {
@@ -25,94 +27,36 @@ export default {
     // 将Vue方法传到全局对象 window 中，使其全局可以调用
     window.openRealPath = this.openRealPath;
     window.newFile = this.newFile;
-    // window.openFile = this.openFile;
-    window.InitEditor = this.InitEditor;
+    window.openFile = this.openFile;
+    window.saveFile = this.saveFile;
+    window.saveAsFile = this.saveAsFile;
+    window.needFilePath = this.needFilePath;
 
-    // 渲染进程接收主进程消息
-    const { ipcRenderer } = require("electron");
-
-    // 需要路径的监听
+    // 需要路径
     ipcRenderer.on("needFilePath", (event, data) => {
-      console.log("传入的data：", data);
-      // 将文件地址存储于 input 元素
-      document.getElementById('input').value = data;
-      console.log("路径确定: ", document.getElementById('input').value);
-      // 直接写入文件
-      // 获取textarea文本内容
-      let textMarkdown = document.getElementsByClassName('input')[0].value;
-      // 获取当前打开文件地址
-      let markdownPath = document.getElementById('input').value;
-      console.log(textMarkdown);
-      fs.writeFile(markdownPath, textMarkdown, error => {
-        if (error) return console.log("写入文件失败，原因是" + error.message);
-        else
-          console.log("写入成功");
-      })
+      console.log("主进程传入data：", data);
+      // 调用需要路径方法
+      needFilePath(data);
+      // 调用保存方法
+      saveFile();
     })
+
     // 新建文件
     ipcRenderer.on("newFile", (event) => {
       newFile();
     });
-    // // 调用 Vue 打开文件
-    // ipcRenderer.on("openFile", (event) => {
-    //   openFile();
-    // });
-    // 使用 Electron 打开文件
+    // 使用Electron的dialog+ipc通信打开文件
     ipcRenderer.on("openFilePath", (event, data) => {
-      console.log(data);
-      // 清理的原因是防止监听函数失效
-      // 清空文件地址
-      document.getElementById('input').value = null;
-      // 清空 open 元素，否则打开文件的监听无法正常运行
-      document.getElementById('open').value = null;
-      // 将文件地址存储于 input 元素
-      document.getElementById('input').value = data;
-      console.log("路径确定: ", document.getElementById('input').value);
-      // 根据路径打开文件
-      this.input = this.readFile(data);
-      console.log("文件地址：", data);
+      console.log("主进程传入data：" + data);
+      openFile(data);
     });
     // 保存文件
     ipcRenderer.on("saveFile", (event) => {
-      // 获取textarea文本内容
-      let textMarkdown = document.getElementsByClassName('input')[0].value;
-      // 获取当前打开文件地址
-      let markdownPath = document.getElementById('input').value;
-      console.log(textMarkdown);
-      // 写入本地文件
-      // 无路径的情况
-      if (markdownPath == '') {
-        console.log('目录为空，需要确定目录')
-        // 向主进程发送请求
-        const { ipcRenderer } = require("electron");
-        ipcRenderer.send("needFilePath");
-      }
-      // 有路径的情况
-      else
-        // 直接写入文件
-        fs.writeFile(markdownPath, textMarkdown, error => {
-          if (error) return console.log("写入文件失败，原因是" + error.message);
-          else
-            console.log("写入成功");
-        })
+      saveFile();
     });
     // 另存文件
     ipcRenderer.on("saveAsFile", (event) => {
-      // 获取textarea文本内容
-      let textMarkdown = document.getElementsByClassName('input')[0].value;
-      // 获取当前打开文件地址
-      let markdownPath = document.getElementById('input').value;
-      console.log(textMarkdown);
-      // 写入本地文件
-      // 无路径的情况
-      console.log('另存文件，需要确定目录')
-      // 向主进程发送请求
-      const { ipcRenderer } = require("electron");
-      ipcRenderer.send("needFilePath");
-
-    });
-    ipcRenderer.on("InitEditor", (event) => {
-      InitEditor();
+      saveAsFile();
     });
   },
   methods: {
@@ -130,40 +74,56 @@ export default {
       xhr.send(null)
       return xhr.status === okStatus ? xhr.responseText : null
     },
+    // needFilePath，需要路径
+    needFilePath(dataPath) {
+      // 将文件地址存储于input元素
+      document.getElementById('input').value = dataPath;
+      console.log("路径确定: ", document.getElementById('input').value);
+    },
     // newFile，新建文件
     newFile: function () {
-      // 清空文件地址
+      // 预防性清空input中储存的文件地址
       document.getElementById('input').value = null;
-      // 清空 open 元素，否则打开文件的监听无法正常运行
-      document.getElementById('open').value = null;
-      console.log("input: ", this.input);
-      // 清空 input
+      console.log("input元素中的路径确定: ", document.getElementById('input').value);
+      // 清空Editor中input的内容
       this.input = '';
-      console.log("文件地址：", document.getElementById('input').value);
+      console.log("Editor中input内容确定: ", this.input);
     },
-    // // openFile，打开文件
-    // openFile: function () {
-    //   // 清理的原因是防止监听函数失效
-    //   // 清空文件地址
-    //   document.getElementById('input').value = null;
-    //   // 清空 open 元素，否则打开文件的监听无法正常运行
-    //   document.getElementById('open').value = null;
-    //   console.log("input: ", this.input);
-    //   // 创建一个 open 变量，用于存储"open"的元素
-    //   var open = document.getElementById("open");
-    //   // 点击事件，打开选择文件窗口
-    //   document.getElementById('open').click();
-    //   // 监听元素变动，一旦有变动便运行 openRealPath
-    //   open.addEventListener('change', openRealPath);
-    //   console.log("执行操作：打开文件")
-    // },
-    // openRealPath，打开真实地址的文件
-    openRealPath: function () {
-      // 储存文件地址
-      document.getElementById('input').value = document.getElementById('open').files[0].path;
-      // 用 readFile 读取新目录下的 md 文件，然后替换 input 的内容
-      this.input = this.readFile(document.getElementById('open').files[0].path);
-      console.log("文件地址：", document.getElementById('open').files[0].path);
+    // openFile，打开文件
+    openFile(dataPath) {
+      // 预防性清空input中储存的文件地址
+      document.getElementById('input').value = null;
+      // 将文件地址存储于 input 元素，方便其他功能调用
+      document.getElementById('input').value = dataPath;
+      console.log("input元素中的路径确定: ", document.getElementById('input').value);
+      // 根据路径打开文件
+      this.input = this.readFile(dataPath);
+      console.log("当前打开文件的地址：", dataPath);
+    },
+    saveFile: function () {
+      // let变量，防止错误的修改这些东西
+      let textMarkdown = document.getElementsByClassName('input')[0].value;
+      let markdownPath = document.getElementById('input').value;
+      // 写入本地文件
+      // 无路径的情况
+      if (markdownPath == '') {
+        console.log('目录为空，需要确定目录')
+        // 向主进程发送请求
+        ipcRenderer.send("needFilePath");
+      }
+      // 有路径的情况
+      else
+        // 直接写入文件
+        fs.writeFile(markdownPath, textMarkdown, error => {
+          if (error) return console.log("写入文件失败，原因是" + error.message);
+          else
+            console.log("写入成功，写入内容为：" + textMarkdown);
+        })
+    },
+    saveAsFile: function () {
+      console.log('另存文件，需要确定目录')
+      // 向主进程发送需要路径请求
+      ipcRenderer.send("needFilePath");
     },
     // 左右滚动条滚动同步
     changeFlag(flag) {
@@ -198,11 +158,8 @@ export default {
 
 <template>
   <!-- input textatra 暂存信息 -->
-  <el-input id="input" type="textarea" :autosize="{ minRows: 1, maxRows: 1 }" placeholder="文件目录" v-model="textarea"
-    style="display:none">
-  </el-input>
-  <!-- 这里的 accept=".md" 可以确保上传文件为 Markdown 文件 -->
-  <input id="open" type="file" accept=".md" name="filename" style="display:none" />
+  <textarea id="input" style="display:none">
+  </textarea>
   <!-- Editor 主体，包含 input 和 output -->
   <div class="editor">
     <!-- 通过ref属性来操控两个div的scrollTop属性，结合 JS 来实现同步滚动 -->
